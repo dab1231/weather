24ab376eef916b0de87d2612a5643ec06687fbb1
dab1231 <popaorlov@gmail.com>
2026-04-09 16:40:56 +0400

    added flyway dependency

--- src/main/java/com/nik/weather/config/SpringConfig.java
+++ src/main/java/com/nik/weather/config/SpringConfig.java
@@ -4,6 +4,7 @@ package com.nik.weather.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -14,6 +15,7 @@ import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan("com.nik.weather")
 public class SpringConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
