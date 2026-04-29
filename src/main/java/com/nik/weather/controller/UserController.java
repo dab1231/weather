c56ab543a27340600177a283998ecfaab4b04014
dab1231 <popaorlov@gmail.com>
2026-04-29 22:29:12 +0400

    finish sign up

--- src/main/java/com/nik/weather/controller/UserController.java
+++ src/main/java/com/nik/weather/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.nik.weather.controller;
 
+import com.nik.weather.dto.request.UserReqDto;
+import com.nik.weather.exception.UserAlreadyExistsException;
 import com.nik.weather.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
@@ -18,9 +23,24 @@ public class UserController {
     }
 
     @GetMapping("/sign-up")
-    public String registration() {
-        return "sign-up-with-errors";
+    public String signUpPage() {
+        return "sign-up";
     }
 
+    @PostMapping("/sign-up")
+    public String signUp(@RequestParam String login,
+                         @RequestParam String password,
+                         Model model) {
 
+        UserReqDto userReqDto = new UserReqDto(login, password);
+        try {
+            userService.registration(userReqDto);
+            return "redirect:/user/sing-in";
+        }
+        catch (UserAlreadyExistsException e) {
+            model.addAttribute("error", "User already exists");
+            return "sign-up";
+        }
+
+    }
 }
