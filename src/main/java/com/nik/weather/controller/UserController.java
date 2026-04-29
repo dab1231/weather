f4bd360b0a799e83fe550bf18a720dd7348498a9
dab1231 <popaorlov@gmail.com>
2026-04-29 23:05:09 +0400

    add sign in

--- src/main/java/com/nik/weather/controller/UserController.java
+++ src/main/java/com/nik/weather/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.nik.weather.controller;
 
 import com.nik.weather.dto.request.UserReqDto;
+import com.nik.weather.exception.InvalidLoginException;
+import com.nik.weather.exception.InvalidPasswordException;
 import com.nik.weather.exception.UserAlreadyExistsException;
 import com.nik.weather.service.UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,12 +39,39 @@ public class UserController {
         UserReqDto userReqDto = new UserReqDto(login, password);
         try {
             userService.registration(userReqDto);
-            return "redirect:/user/sing-in";
+            return "redirect:/user/sign-in";
         }
         catch (UserAlreadyExistsException e) {
             model.addAttribute("error", "User already exists");
             return "sign-up";
         }
+    }
 
+    @GetMapping("/sign-in")
+    public String signInPage() {
+        return "sign-in";
+    }
+
+    @PostMapping("/sign-in")
+    public String signIn(
+            @RequestParam String login,
+            @RequestParam String password,
+            HttpServletResponse httpServletResponse,
+            Model model) {
+
+        try {
+            UserReqDto userReqDto = new UserReqDto(login, password);
+            var sessionRespDto = userService.login(userReqDto);
+            var cookie = new Cookie("session_id", sessionRespDto.id().toString());
+            cookie.setMaxAge(24 * 60 * 60);
+            httpServletResponse.addCookie(cookie);
+            return "redirect:/home";
+        } catch (InvalidPasswordException e) {
+            model.addAttribute("error", "Invalid password");
+            return "sign-in";
+        } catch (InvalidLoginException e) {
+            model.addAttribute("error", "Invalid username");
+            return "sign-in";
+        }
     }
 }
