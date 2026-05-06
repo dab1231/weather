7473080d77ad358825a71f12414c17b8b02d45c9
dab1213 <popaorlov@gmail.com>
2026-05-06 14:45:48 +0400

    refactor WeatherRespDto

--- src/main/java/com/nik/weather/dto/response/WeatherRespDto.java
+++ src/main/java/com/nik/weather/dto/response/WeatherRespDto.java
@@ -1,4 +1,16 @@
 package com.nik.weather.dto.response;
 
-public record WeatherRespDto(String temp, String feelsLike, String humidity, String icon) {
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class WeatherRespDto {
+    private Main main;
+    private List<Weather> weather;
+
+    public record Main(double temp,
+                       @SerializedName("feels_like") double feelsLike,
+                       int humidity) {}
+
+    public record Weather(String icon, String description) {}
 }
