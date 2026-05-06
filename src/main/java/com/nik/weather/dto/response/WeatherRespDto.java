a4f0e1f43c54b83b92033f403aec63a3ba8a8cb3
dab1213 <popaorlov@gmail.com>
2026-05-06 14:37:03 +0400

    add getWeather

---
+++ src/main/java/com/nik/weather/dto/response/WeatherRespDto.java
@@ -0,0 +1,4 @@
+package com.nik.weather.dto.response;
+
+public record WeatherRespDto(String temp, String feelsLike, String humidity, String icon) {
+}
--- src/main/java/com/nik/weather/service/WeatherService.java
+++ src/main/java/com/nik/weather/service/WeatherService.java
@@ -3,16 +3,20 @@ package com.nik.weather.service;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.nik.weather.dto.response.CitiesRespDto;
+import com.nik.weather.dto.response.WeatherRespDto;
 import com.nik.weather.exception.WeatherApiException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class WeatherService {
@@ -20,6 +24,7 @@ public class WeatherService {
     @Value("${openweather.api.key}")
     private String apiKey;
     private final Gson gson;
+    private final HttpClient httpClient = HttpClient.newHttpClient();
 
     @Autowired
     public WeatherService(Gson gson) {
@@ -29,7 +34,7 @@ public class WeatherService {
     public CitiesRespDto[] findCity(String cityName) {
 
         try {
-            var httpClient = HttpClient.newHttpClient();
+            cityName = URLEncoder.encode(cityName, StandardCharsets.UTF_8);
             var request = HttpRequest.newBuilder(
                     URI.create("http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=" + apiKey)).build();
             var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
@@ -37,6 +42,22 @@ public class WeatherService {
 
             return gson.fromJson(respString, CitiesRespDto[].class);
 
+        } catch (IOException | InterruptedException | JsonSyntaxException e) {
+            throw new WeatherApiException();
+        }
+    }
+
+    public WeatherRespDto getWeather(BigDecimal lat, BigDecimal lon) {
+
+        try {
+            var httpRequest = HttpRequest.newBuilder(
+                    URI.create("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey)
+            ).build();
+
+            var response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            var respString = response.body();
+
+            return gson.fromJson(respString, WeatherRespDto.class);
         } catch (IOException | InterruptedException | JsonSyntaxException e) {
             throw new WeatherApiException();
         }
